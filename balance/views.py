cf0d05df0b7254a398e161a9adafdfcac7aeac82
Alejo Rios <alejoriosiglesias@gmail.com>
2022-07-11 20:10:39 +0100

    feat: parametros en la URL

--- balance/views.py
+++ balance/views.py
@@ -1,17 +1,20 @@
 from . import app
 
-@app.route('/')
-def home():
-    return "Homepage"
+@app.route("/")
+def inicio():
+    return "Página de inicio"
 
-@app.route('/nuevo', methods=['GET', 'POST'])
+
+@app.route("/nuevo", methods=["GET", "POST"])
 def nuevo():
-    return "crear movimiento"
+    return "Crear movimiento"
 
-@app.route('/modificar')
-def actualizar():
-    return "actualizar movimiento"
 
-@app.route('/borrar', methods=['GET', 'POST'])
-def eliminar():
-    return "eliminar movimiento"
+@app.route("/modificar/<int:id>", methods=["GET", "POST"])
+def actualizar(id):
+    return f"Actualizar el movimiento con ID={id}"
+
+
+@app.route("/borrar/<int:id>", methods=["GET", "POST"])
+def eliminar(id):
+    return f"Eliminar el movimiento {id}"
