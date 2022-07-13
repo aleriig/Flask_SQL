ed2f531ea75787dbcf5db8c4063e070ea5c13d72
Alejo Rios <alejoriosiglesias@gmail.com>
2022-07-13 22:10:07 +0100

    feat: agregamos validaciones a todos los campos

--- balance/forms.py
+++ balance/forms.py
@@ -6,10 +6,14 @@ from wtforms.validators import DataRequired, Length
 class MovimientosForm(FlaskForm):
     # ocultamos el id
     id = HiddenField
+    
     fecha = DateField("Fecha", validators=[DataRequired(message="Debes introducir una fecha")])
-    concepto = StringField("Concepto", validators=[DataRequired()])
-    tipo = RadioField(choices=[("I", "Ingreso"), ("G", "Gasto")])
-    cantidad = FloatField("Cantidad")
+    
+    concepto = StringField("Concepto", validators=[DataRequired(message="Debes introducir un concepto"), Length(min=3, max=25, message="Debe tener entre 3 y 25 caracteres")])
+    
+    tipo = RadioField(choices=[("I", "Ingreso"), ("G", "Gasto")],validators=[DataRequired(message="¿Es un gasto o un ingreso?")])
+    
+    cantidad = FloatField("Cantidad", validators=[DataRequired(message="Debes especificar un número")])
 
     submit = SubmitField("Guardar", render_kw={ "class : blue-button" })
     
